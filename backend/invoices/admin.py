2038180b022b51cb54cda33146f07a0de52b8a89
Rashid-C <anurashid11055@gmail.com>
2026-06-07 16:22:43 +0530

    fixed Indentation error

--- backend/invoices/admin.py
+++ backend/invoices/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
-from .models import Tailor, Invoice,RateSheet
+from .models import Tailor, Invoice, RateSheet
 
 @admin.register(Tailor)
 class TailorAdmin(admin.ModelAdmin):
     list_display = ['code', 'name', 'phone', 'created_at']
     search_fields = ['code', 'name']
-
-    @admin.register(RateSheet)
-class RateSheetAdmin(admin.ModelAdmin):
-    list_display = ['md_no', 'tailor', 'rate', 'work_type', 'is_active']
-    search_fields = ['md_no', 'tailor__code']
-    list_filter = ['tailor', 'is_active', 'work_type']
 
 @admin.register(Invoice)
 class InvoiceAdmin(admin.ModelAdmin):
@@ -18,3 +12,9 @@ class InvoiceAdmin(admin.ModelAdmin):
     search_fields = ['inv_no', 'tailor__code', 'md_no']
     list_filter = ['tailor', 'rcv_date', 'is_return']
     readonly_fields = ['amount']
+
+@admin.register(RateSheet)
+class RateSheetAdmin(admin.ModelAdmin):
+    list_display = ['md_no', 'tailor', 'rate', 'work_type', 'is_active']
+    search_fields = ['md_no', 'tailor__code']
+    list_filter = ['tailor', 'is_active', 'work_type']
